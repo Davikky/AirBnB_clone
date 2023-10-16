b5ab1c598d3f8a2600991ef45a48b74fb2e7433a
Davikky <ayobamivikky@gmail.com>
2023-10-16 06:44:58 -0700

    Updated BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -49,14 +49,17 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
-            # Convert 'created_at' and 'updated_at' string to datetime
+                        """
+                        Convert 'created_at' and 'updated_at'
+                        string to datetime
+                        """
                         setattr(self, key, datetime.fromisoformat(value))
                     else:
                         setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at =self.created_at
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Return a string representation of the instance."""
